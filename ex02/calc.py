98d374eb65bed8a615e6be84bb64cfbdbdd7d941
sugahara_rintaro <c0b21089cb@edu.teu.ac.jp>
2022-10-04 14:03:02 +0900

    add clac button

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,4 +5,9 @@ root = tk.Tk()
 root.title('clac')
 root.geometry('300x500')
 
+for i in range(9, -1, -1):
+    index = 9 - i
+    button = tk.Button(root, text=str(i),width=4, height=2, font=('Times New Roman', 30))
+    button.grid(column=index%3, row=index//3)
+
 root.mainloop()
