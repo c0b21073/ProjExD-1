6b34cde922865f75369ea468a020441851a9662b
sugahara_rintaro <c0b21089cb@edu.teu.ac.jp>
2022-10-18 14:20:00 +0900

    add show koukaton

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,4 +2,14 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("迷えるこうかとん")
+
+canvas = tk.Canvas(root, width=1500, height=900, bg='black')
+canvas.pack()
+
+tori = tk.PhotoImage(file='ex03/fig/5.png')
+cx,cy = 300, 400
+canvas.create_image(cx, cy, image=tori, tag='tori')
+
+key = ""
+
 root.mainloop()
