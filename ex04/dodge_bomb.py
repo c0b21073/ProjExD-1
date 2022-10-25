9002c06f62c0d8962314fc864b2e85426b2d0403
sugahara_rintaro <c0b21089cb@edu.teu.ac.jp>
2022-10-25 14:51:25 +0900

    add move limit

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -23,6 +23,7 @@ def main():
     clock = pg.time.Clock()
     tori_sfc, tori_rct = make_img("mt/fig/6.png", 900,400, m=2.0)
     bomb_x, bomb_y = randint(10,1590), randint(10, 890)
+    vx, vy = 1, 1
 
 
     while True:
@@ -33,15 +34,28 @@ def main():
                 return
 
         key_list = pg.key.get_pressed()
-        if key_list[pg.K_UP]: tori_rct.centery -= 1
-        if key_list[pg.K_DOWN]: tori_rct.centery += 1
-        if key_list[pg.K_RIGHT]: tori_rct.centerx += 1
-        if key_list[pg.K_LEFT]: tori_rct.centerx -= 1
+        if key_list[pg.K_UP]: 
+            if 0 < tori_rct.top: 
+                tori_rct.centery -= 1
+        if key_list[pg.K_DOWN]: 
+            if tori_rct.bottom < 900:
+                tori_rct.centery += 1
+        if key_list[pg.K_RIGHT]: 
+            if tori_rct.right < 1600:
+                tori_rct.centerx += 1
+        if key_list[pg.K_LEFT]: 
+            if 0 < tori_rct.left:
+                tori_rct.centerx -= 1
 
         scrn_sfc.blit(tori_sfc, tori_rct)
         scrn_sfc.blit(make_bomb(), (bomb_x,bomb_y))
-        bomb_x += 1
-        bomb_y += 1
+        if not 10 <= bomb_x <= 1590:
+            vx *= -1
+        bomb_x += vx
+        if not 10 <= bomb_y <= 890:
+            vy *= -1
+        bomb_y += vy
+
         pg.display.update()
         clock.tick(1000)
 
