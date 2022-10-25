e6246eef120d05d8e8f82130a4955ec5f86c8f35
sugahara_rintaro <c0b21089cb@edu.teu.ac.jp>
2022-10-25 14:40:00 +0900

    add bomb

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+from random import randint
 
 def make_img(path, x, y, r=0, m=1.0):
     sfc = pg.image.load(path)
@@ -8,12 +9,21 @@ def make_img(path, x, y, r=0, m=1.0):
     rct.center = x, y
     return sfc, rct
 
+def make_bomb():
+    sfc = pg.Surface((20,20))
+    pg.draw.circle(sfc,(255,0,0),(10,10), 10)
+    sfc.set_colorkey('black')
+    return sfc
+
+
 def main():
     pg.display.set_caption("逃げろ!こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
     back_sfc = pg.image.load('mt/pg_bg.jpeg')
     clock = pg.time.Clock()
     tori_sfc, tori_rct = make_img("mt/fig/6.png", 900,400, m=2.0)
+    bomb_x, bomb_y = randint(10,1590), randint(10, 890)
+
 
     while True:
         scrn_sfc.blit(back_sfc, (0,0))
@@ -23,16 +33,15 @@ def main():
                 return
 
         key_list = pg.key.get_pressed()
-        if key_list[pg.K_UP]:
-            tori_rct.centery -= 1
-        if key_list[pg.K_DOWN]:
-            tori_rct.centery += 1
-        if key_list[pg.K_RIGHT]:
-            tori_rct.centerx += 1
-        if key_list[pg.K_LEFT]:
-            tori_rct.centerx -= 1
+        if key_list[pg.K_UP]: tori_rct.centery -= 1
+        if key_list[pg.K_DOWN]: tori_rct.centery += 1
+        if key_list[pg.K_RIGHT]: tori_rct.centerx += 1
+        if key_list[pg.K_LEFT]: tori_rct.centerx -= 1
 
         scrn_sfc.blit(tori_sfc, tori_rct)
+        scrn_sfc.blit(make_bomb(), (bomb_x,bomb_y))
+        bomb_x += 1
+        bomb_y += 1
         pg.display.update()
         clock.tick(1000)
 
