015deffcbec211fcfe99801ae4f8c32cdcfafaed
sugahara_rintaro <c0b21089cb@edu.teu.ac.jp>
2022-10-25 14:24:02 +0900

    add move kokaton

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,26 +1,38 @@
 import pygame as pg
 import sys
 
-def make_img(main_sfc, path, x, y, r=0, m=1.0):
+def make_img(path, x, y, r=0, m=1.0):
     sfc = pg.image.load(path)
     sfc = pg.transform.rotozoom(sfc, r, m)
     rct = sfc.get_rect()
     rct.center = x, y
-    main_sfc.blit(sfc, rct)
+    return sfc, rct
 
 def main():
     pg.display.set_caption("逃げろ!こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
     back_sfc = pg.image.load('mt/pg_bg.jpeg')
     clock = pg.time.Clock()
-    scrn_sfc.blit(back_sfc, (0,0))
-    make_img(scrn_sfc, "mt/fig/6.png", 900,400, m=2.0)
-
+    tori_sfc, tori_rct = make_img("mt/fig/6.png", 900,400, m=2.0)
 
     while True:
+        scrn_sfc.blit(back_sfc, (0,0))
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+
+        key_list = pg.key.get_pressed()
+        if key_list[pg.K_UP]:
+            tori_rct.centery -= 1
+        if key_list[pg.K_DOWN]:
+            tori_rct.centery += 1
+        if key_list[pg.K_RIGHT]:
+            tori_rct.centerx += 1
+        if key_list[pg.K_LEFT]:
+            tori_rct.centerx -= 1
+
+        scrn_sfc.blit(tori_sfc, tori_rct)
         pg.display.update()
         clock.tick(1000)
 
